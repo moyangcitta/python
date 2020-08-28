e2bd4840f211ac07e19f9223006a939b01719bf5
moyangcitta <2033402740@qq.com>
2020-08-28 16:40:11 +0800

    code

---
+++ practice.py
@@ -0,0 +1,23 @@
+
+
+# fname = input("请输入要写入的文件：")
+# fo = open(fname,"w+")
+# ls = ["唐诗","宋词","元曲"]
+# fo.writelines(ls)
+# fo.seek(0)
+# for line in fo:
+#     print(line)
+# fo.close()
+
+# fname = input("请输入要打开的文件：")
+# fo = open(fname,"r")
+# for line in fo.readlines():
+#     print(line)
+# fo.close()
+
+# textFile = open("7.1.txt","rt")     #t表示文本文件方式
+# print(textFile.readline())
+# textFile.close()
+# binFile = open("7.1.txt","rb")      #r表示二进制文件方式
+# print(binFile.readline())
+# binFile.close()
