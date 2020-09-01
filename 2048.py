a08cacbec07abb974fe2f7bc2288ecdd1f942569
moyangcitta <2033402740@qq.com>
2020-09-01 09:13:55 +0800

    game

--- 2048.py
+++ 2048.py
@@ -2,27 +2,33 @@
 #-*- coding:utf-8 -*-
 #practice_2048
 import random
-location = []
-for i in range(16):
-    location.append(0)
+location = [[0,0,0,0],                                 #建立基础列表
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0]]
 
-def number_make():                                  #生成随机数字
-    while 1:                                        #循环消除相同位置的数字
-        number_location_f = random.random() * 16    #产生随机位置
-        number_location_c = "{0:.0f}".format(number_location_f + 1)
-        number_location = eval(number_location_c)
-        if location[number_location] != 0:          #判断是否存在相同数字
+def number_make():                                      #生成随机数字
+    while 1:                                            #循环消除相同位置的数字
+        number_location_f_l = random.random() * 4       #产生随机列位置
+        number_location_c_l = "{0:.0f}".format(number_location_f_l + 1)
+        number_location_l = eval(number_location_c_l)
+        number_location_f_h = random.random() * 4       #产生随机行位置
+        number_location_c_h = "{0:.0f}".format(number_location_f_h + 1)
+        number_location_h = eval(number_location_c_h)
+        location_h = location[number_location_h]
+        if location_h[number_location_l] != 0:          #判断是否存在相同数字
             continue
         else:
-            location[number_location] = 2
-        for i in range(16):                         #画出数字所在位置
-            if i in [3,7,11]:
-                print("\n")
-            else:
-                print("{}\t".format(location[number_location]),end = "")
-                break
+            location_h[number_location_l] = 2
+            location[number_location_h] = location_h
+        for i in range(4):                              #画出数字所在位置
+            location_h = location[1]
+            for j in range(4):
+                print("{0:.0f}\t".format(location_h[j]),end = "")
+            print("\n")
 
-def direction_control(a):
+def direction_control(a):                               #方向控制
+    if a == "w":
 
 
 def main():
