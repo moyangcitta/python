3ce5bedd6f506a7d0e179ed06dfb6ff87a28ba2a
moyangcitta <2033402740@qq.com>
2020-09-05 19:12:42 +0800

    game

--- 2048.py
+++ 2048.py
@@ -22,14 +22,57 @@ def number_make():                                      #生成随机数字
             location_h[number_location_l] = 2
             location[number_location_h] = location_h
         for i in range(4):                              #画出数字所在位置
-            location_h = location[1]
+            location_h = location[i]
             for j in range(4):
                 print("{0:.0f}\t".format(location_h[j]),end = "")
             print("\n")
 
-def direction_control(a):                               #方向控制
-    if a == "w":
+def count_h():                                          #计算每次行移动之和,提交行方向和列方向，判断是否有相同数字，完成功能
+    for i in range(3):
+        count_1 = location[i]
+        count_2 = location[i + 1]
+        for j in count_1,count_2:
+            if count_1[j] == count_2[j]:
+                count_1[j] = count_1[j] + count_2[j]
+            elif count_1[j] == 0:
+                count_1[j] == count_2[j]
+            else:
+                continue
+        i += 1
+    for i in range(3):
+        count_1 = location[i]
+        count_2 = location[i + 1]
+        for j in count_1,count_2:
+            if count_1[j] == 0:
+                count_1[j] == count_2[j]
+            else:
+                continue
 
+def count_l():                                          #计算每次列移动之和,提交行方向和列方向，完成功能
+    for i in location:
+        count = location[i]
+        for j in range(3):
+            if count[j] == count[j+1]:
+                count[j] = count[j] + count[j+1]
+            elif count[j] == 0:
+                count[j] == count[j+1]
+            else:
+                continue
+        i += 1
+    for i in location:
+        count = location[i]
+        for j in range(3):
+            if count[j] == 0:
+                count[j] == count[j+1]
+            else:
+                continue
+
+def direction_control(a):                               #方向控制
+    if a == "w":                                        #自顶向下，列方向相加,先提交行，在提交列
+
+    elif a == "a":
+    elif a == "s":
+    elif a == "d":
 
 def main():
     while 1:
